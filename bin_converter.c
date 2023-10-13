3c3e9ecb3dddb664786495eb25d1ea66c2f4128f
Leratorikki <richardmarcus000@gmail.com>
2023-10-13 22:24:29 +0100

    Bin converter.c

---
+++ bin_converter.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * bin_converter - prints decimal in binary
+ * @bin: pointer to binary
+ * @int_input: input number
+ * @isnegative: if input number is negative
+ * @lmt: size of the binary
+ * Return: number of chars printed.
+ */
+char bin_converter(char *bin, long int int_input, int isnegative, int lmt)
+
+	int i;
+
+	for (i = 0; i < lmt; i++)
+		bin[i] = '0';
+	bin[lmt] = '\0';
+	for (i = lmt - 1; int_in > 1; i--)
+	{
+		if (int_in == 2)
+			bin[i] = '0';
+		else
+			bin[i] = (int_input % 2) + '0';
+		int_input /= 2;
+	}
+	if (int_input != 0)
+		bin[i] = '1';
+	if (isnegative)
+	{
+		for (i = 0; bin[i]; i++)
+			if (bin[i] == '0')
+				bin[i] = '1';
+			else
+				bin[i] = '0';
+	}
+	return (bin);
+}
