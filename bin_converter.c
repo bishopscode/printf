48794e22d2257f8c7c035873a87c178c1ff29621
Leratorikki <richardmarcus000@gmail.com>
2023-10-14 06:06:05 +0100

    bin converter

--- bin_converter.c
+++ bin_converter.c
@@ -15,9 +15,9 @@ char bin_converter(char *bin, long int int_input, int isnegative, int lmt)
 	for (i = 0; i < lmt; i++)
 		bin[i] = '0';
 	bin[lmt] = '\0';
-	for (i = lmt - 1; int_in > 1; i--)
+	for (i = lmt - 1; int_input > 1; i--)
 	{
-		if (int_in == 2)
+		if (int_input == 2)
 			bin[i] = '0';
 		else
 			bin[i] = (int_input % 2) + '0';
