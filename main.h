bc56eb52a28ab2c8184a70a21af78ea8ea0d0381
Leratorikki <richardmarcus000@gmail.com>
2023-10-14 08:09:32 +0100

    merging repo

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef main_h
 #define main_h
 
-char bin_converter(char *bin, long int int_input, int isnegative, int lmt) 
-
-
+char *load_bin_converter(char *bin, long int int_input, int isnegative, int lmt); 
+char *load_long_oct_number(char *bin, char *octal);
+char *load_short_octal_print(char *bin, char *octal);
 
 
 
