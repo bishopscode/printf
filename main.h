84226f29cd68d4a7c80272a9ad53306f5d39bc01
Moses Nebechukwu <bishopslow01@gmail.com>
2023-10-14 07:33:46 +0100

    updated addr

--- main.h
+++ main.h
@@ -1,10 +1,8 @@
 #ifndef main_h
 #define main_h
 
-char bin_converter(char *bin, long int int_input, int isnegative, int lmt) 
-
-
-
+char bin_converter(char *bin, long int int_input, int isnegative, int lmt);
+unsigned int com_buff(char *buff, char c, unsigned int ibuff);
 
 
 
