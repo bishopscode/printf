fcce6419be58528059c6ffb5548f254c35520de2
bishopscode <64908269+bishopscode@users.noreply.github.com>
2023-10-15 18:38:48 +0100

    Update short_hex_print_util.c

--- short_hex_print_util.c
+++ short_hex_print_util.c
@@ -35,7 +35,7 @@ int prthhex(va_list arguments, char *buff, unsigned int ibuff)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuff = conm_buff(buff, hexadecimal[i], ibuff);
+			ibuff = com_buff(buff, hexadecimal[i], ibuff);
 			count++;
 		}
 	}
