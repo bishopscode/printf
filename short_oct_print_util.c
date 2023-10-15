b5a180b659d5173e17e073545ad45eb8c24a87a0
Moses Nebechukwu <bishopslow01@gmail.com>
2023-10-15 21:14:26 +0100

    update short_oct_util

--- short_oct_print_util.c
+++ short_oct_print_util.c
@@ -28,7 +28,7 @@ int prthoct(va_list arguments, char *buff, unsigned int ibuff)
 	binary = malloc(sizeof(char) * (16 + 1));
 	binary = load_binary_conv(binary, int_input, isnegative, 16);
 	octal = malloc(sizeof(char) * (6 + 1));
-	octal = load_short_oct_conv(binary, octal);
+	octal = load_short_octal_conv(binary, octal);
 	for (first_digit = i = count = 0; octal[i]; i++)
 	{
 		if (octal[i] != '0' && first_digit == 0)
