f1984a865f575d65fd987b5ee6b270295a802980
Moses Nebechukwu <bishopslow01@gmail.com>
2023-10-15 19:13:34 +0100

    taking shape

--- main.h
+++ main.h
@@ -53,7 +53,7 @@ int id_print_func(const char *s, int index);
 unsigned int com_buff(char *buff, char c, unsigned int ibuff);
 int print_buff(char *buff, unsigned int nbuff);
 char *load_binary_conv(char *binary, long int int_in, int isnegative, int lmt);
-char *load_oct_conv(char *bin, char *oct);
+char *load_octal_conv(char *bin, char *oct);
 char *load_long_oct_conv(char *bin, char *oct);
 char *load_short_oct_conv(char *bin, char *oct);
 char *load_hex_conv(char *bin, char *hex, int isupp, int lmt);
