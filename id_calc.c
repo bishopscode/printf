d4484ba695b023a2d09152311c3072ea5cf1f467
bishopscode <64908269+bishopscode@users.noreply.github.com>
2023-10-15 18:22:36 +0100

    Update id_calc.c

--- id_calc.c
+++ id_calc.c
@@ -12,7 +12,7 @@ int id_print_func(const char *s, int index)
 		{"c", print_char}, {"s", print_str}, {"i", print_int},
 		{"d", print_int}, {"b", print_bin}, {"u", print_unt},
 		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
-		{"S", print_usr}, {"p", print_add}, {"li", prtlint},
+		{"S", print_usr}, {"p", print_addr}, {"li", prtlint},
 		{"ld", prtlint}, {"lu", prtlunt}, {"lo", prtloct},
 		{"lx", prtlhex}, {"lX", prtlupx}, {"hi", prthint},
 		{"hd", prthint}, {"hu", prthunt}, {"ho", prthoct},
