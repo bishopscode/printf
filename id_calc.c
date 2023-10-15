5b4c38f36e06e3b7fff5a1ada46f1f21d29966a6
Moses Nebechukwu <bishopslow01@gmail.com>
2023-10-15 16:59:47 +0100

    debug

--- id_calc.c
+++ id_calc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * id_calc - returns the amount of identifiers.
+ * id_print_func - returns the amount of identifiers.
  * @s: argument indentifier
  * @index: index of argument identifier.
  * Return: amount of identifiers.
