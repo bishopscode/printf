6e49d51d651db1748020d19c2158b4445a09e0b6
bishopscode <64908269+bishopscode@users.noreply.github.com>
2023-10-16 13:05:58 +0100

    Update load_short_octal_conv.c

--- load_short_octal_conv.c
+++ load_short_octal_conv.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * load_short_oct_conv - the calculation of a short octal number
- * @bin: arr that is stored in the binary.
- * @oct: the array that is stored in the octal.
+ * load_short_oct_conv - calculates a short octal number
  *
- * Return: bin array.
+ * @bin: array where is stored the binary.
+ * @oct: array where is stored the octal.
+ *
+ * Return: binary array.
  */
 char *load_short_oct_conv(char *bin, char *oct)
 {
-	int og, i, j, ioctal, lmt;
+	int og, i, j, ioct, lmt;
 
 	oct[6] = '\0';
 	for (i = 15, ioct = 5; i >= 0; i--, ioct--)
@@ -18,7 +19,7 @@ char *load_short_oct_conv(char *bin, char *oct)
 			lmt = 4;
 		else
 			lmt = 1;
-		for (og = 0, j = 1; j <= lmt; j *= 2, i--)
+		for (og = 0, j = 1; j <= limit; j *= 2, i--)
 			og = ((bin[i] - '0') * j) + og;
 		i++;
 		oct[ioct] = og + '0';
